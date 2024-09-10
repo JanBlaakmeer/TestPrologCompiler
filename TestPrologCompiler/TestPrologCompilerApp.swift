70e02c7252a271e7d75d7bbfca8e332588e0ecd1
Jan Blaakmeer <jan@blaakmeer.nl>
2024-09-10 10:50:26 +0200

    Initial Commit

---
+++ TestPrologCompiler/TestPrologCompilerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestPrologCompilerApp.swift
+//  TestPrologCompiler
+//
+//  Created by Jan Blaakmeer on 10/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestPrologCompilerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
